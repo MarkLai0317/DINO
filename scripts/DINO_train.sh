7e30252c811a1962d7042a9315b16b4d960ca0da
MarkLai0317 <laimark900317@gmail.com>
2023-10-05 23:32:16 +0800

    modify script and config

--- scripts/DINO_train.sh
+++ scripts/DINO_train.sh
@@ -3,4 +3,6 @@ python main.py \
 	--output_dir logs/DINO/R50-MS4 -c config/DINO/DINO_4scale.py --coco_path $coco_path \
 	--options dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
-	dn_box_noise_scale=1.0
+	dn_box_noise_scale=1.0 \
+    --pretrain_model_path pretrain_model/checkpoint0011_4scale.pth \
+    --finetune_ignore label_enc.weight class_embed
